10ce547186784487c1f4d555a30ab023ab3030d7
Nokhalal Mahato <nokhalal412000@gmail.com>
2022-03-27 16:18:39 +0530

    commit message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+	const DUMMY_expenses=[
+	{
+		id:'e1',
+		title:"House rent",
+		date:new Date(2022,3,21),
+		amount:5000,
+	},
+	{
+		id:'e2',
+		title:"Car EMI",
+		date:new Date(2022,2,21),
+		amount:25000,
+	},
+	{
+		id:'e3',
+		title:"New Phone",
+		date:new Date(2020,4,21),
+		amount:45000,
+	},
+	{
+		id:'e4',
+		title:"Electricity Bill",
+		date:new Date(2022,1,21),
+		amount:2000,
+	},
+	];
+
+	const [expenses,setExpenses]=useState(DUMMY_expenses);
+
+	const addExpenseHandler = (expense)=>{
+		setExpenses((prevExpense)=>{
+			return [expense,...prevExpense]
+		});
+	};
+
+	return (
+		<div>
+            <NewExpense onAddExpense={addExpenseHandler}/>
+            <Expenses items={expenses}/>
+		</div>
+	)
 }
 
 export default App;
---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,16 @@
+import ChartBar from "./CharBar";
+import './Chart.css';
+
+const Chart = (props)=>{
+    const dataPointValues=props.dataPoints.map(dataPoint=>dataPoint.value);
+    const totalMax=Math.max(...dataPointValues);
+    return(
+        <div className="chart">
+            {props.dataPoints.map(dataPoint => {
+            return <ChartBar key={dataPoint.label} value={dataPoint.value} label={dataPoint.label} maxValue={totalMax} />
+            })}
+        </div>
+    );
+};
+
+export default Chart;
